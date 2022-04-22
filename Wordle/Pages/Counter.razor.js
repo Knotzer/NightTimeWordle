7e5e901209edff03cae3ee651e5cbdd4c9b6c642
Knotzer <fazbeardeper@gmail.com>
2022-04-21 21:59:55 -0600

    added jakyll file

--- Wordle/Pages/Counter.razor.js
+++ Wordle/Pages/Counter.razor.js
@@ -1,37 +1,41 @@
-﻿function PlaySong(Choice, Songs) {
-    var Choice = Math.floor(Math.random() * 16);
-    var sound = new Howl({
+﻿songs = [
+"/Music/__.mp3",
+"/Music/90's flav - call me.mp3",
+"/Music/Brandon Kai - 3 AM.mp3",
+"/Music/Flance - Somdedays",
+"/Music/j^p^n - amend (second part looped).mp3",
+"/Music/Kanisan x Wishes and Dreams - morning moon.mp3",
+"/Music/kudasai - dream of her.mp3",
+"/Music/kudasai - technicolor.mp3",
+"/Music/kudasai - the girl i haven't met.mp3",
+"/Music/Lavender.mp3",
+"/Music/Let Me Hold You.mp3",
+"/Music/Melatonin.mp3",
+"/Music/Ocean I.mp3",
+"/Music/Peace of Mind.mp3",
+"/Music/Stargazing.mp3",
+];
 
-        src: [Songs[Choice]],
-        html5: true,
-        onend: function () {
-            PlaySong(Choice, Songs)
-        }
-    });
-
-    sound.play();
+const getSongUrl = () => {
+    var choice = Math.floor(Math.random() * songs.length);
+    return songs[choice];
 }
 
-var Choice = Math.floor(Math.random()* 16);
-const Songs = [
-    "/Music/__.mp3",
-    "/Music/90's flav - call me.mp3",
-    "/Music/Brandon Kai - 3 AM.mp3",
-    "/Music/Flance - Somdedays",
-    "/Music/j^p^n - amend (second part looped).mp3",
-    "/Music/Kanisan x Wishes and Dreams - morning moon.mp3",
-    "/Music/kudasai - dream of her.mp3",
-    "/Music/kudasai - technicolor.mp3",
-    "/Music/kudasai - the girl i haven't met.mp3",
-    "/Music/Lavender.mp3",
-    "/Music/Let Me Hold You.mp3",
-    "/Music/Melatonin.mp3",
-    "/Music/Ocean I.mp3",
-    "/Music/Peace of Mind.mp3",
-    "/Music/Stargazing.mp3",
+const playNextSong = () => {
+    sound.src = getSongUrl();
+}
 
-];
-PlaySong(Choice, Songs);
+const sound = new Howl({
+    src: getSongUrl(),
+    html5: true,
+    onend: function () {
+        playNextSong();
+    }
+});
+
+
+
+
 
 
 
